4d27f15aae9c5bb2c1848891d00e212290e980e0
warlck <addick@me.com>
2014-01-28 18:33:31 +0800

    Add point_line_item specs and create point_expirer spec file

--- app/models/point_line_item.rb
+++ app/models/point_line_item.rb
@@ -1,2 +1,3 @@
 class PointLineItem < ActiveRecord::Base
+	belongs_to :user
 end
--- spec/models/point_line_item_spec.rb
+++ spec/models/point_line_item_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe PointLineItem do
+	it { should respond_to(:points)}
+	it { should respond_to(:source)}
+	it { should respond_to(:created_at)}
+
+	it { should belong_to(:user)}
+
+	it { should have_db_index(:user_id)}
+	it { should have_db_index(:created_at)}
   
 end
