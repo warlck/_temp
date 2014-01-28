b0d5ad79ff06c590964e9edd9d17fd480ebbb131
warlck <addick@me.com>
2014-01-28 17:24:43 +0800

    create PointLineItem model

--- db/migrate/20140128091608_create_point_line_items.rb
+++ db/migrate/20140128091608_create_point_line_items.rb
@@ -1,8 +1,10 @@
 class CreatePointLineItems < ActiveRecord::Migration
   def change
     create_table :point_line_items do |t|
-
-      t.timestamps
+      t.belongs_to  :user
+      t.integer     :points
+      t.string      :source
+      t.datetime    :created_at
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128090013) do
+ActiveRecord::Schema.define(version: 20140128091608) do
+
+  create_table "point_line_items", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "points"
+    t.string   "source"
+    t.datetime "created_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
