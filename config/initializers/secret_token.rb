4e61499426d0106de86fd812b6600b6818a2dddf
warlck <addick@me.com>
2014-01-28 14:27:37 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Luxola::Application.config.secret_key_base = 'b344fe4aaf02e76a70ba2150fa923f54c94e56180505c0e6a7345245c764fd8eb0ad0fa90af96382850099a8ee5558e7ff908f2d323fac977a7e01443dc613a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Luxola::Application.config.session_store :cookie_store, key: '_luxola_session'
