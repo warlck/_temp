bfca0e64f99672a9f4fd4435cec86526b5e7fad8
warlck <addick@me.com>
2014-02-05 20:00:47 +0800

    Populate seeds with user and point line items as described in task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+adam = User.create(name: "Adam")
+
+PointLineItem.create(user: adam, points:   25,  source: "Joined Loyalty Program",    created_at: '01/01/2013')
+PointLineItem.create(user: adam, points:  410,  source: "Placed an order",           created_at: '10/02/2013')
+PointLineItem.create(user: adam, points: -250,  source: "Redeemed with a an order",  created_at: '15/02/2013')
+PointLineItem.create(user: adam, points:   10,  source: "Wrote a review",            created_at: '18/02/2013')
+PointLineItem.create(user: adam, points:  570,  source: "Placed an order",           created_at: '12/03/2013')
+PointLineItem.create(user: adam, points: -500,  source: "Redeemed with an order",    created_at: '15/04/2013')
+PointLineItem.create(user: adam, points:  130,  source: "Made a purchase",           created_at: '27/06/2013')
+
+
+
